6ec02f43fe227e998d2975fd5389b953ac88d65b
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2025-02-22 11:13:59 +0500

    commit

--- src/router/ProtectedRouterLayout.tsx
+++ src/router/ProtectedRouterLayout.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux"
-import { Navigate, Outlet } from "react-router"
+import { Navigate, Outlet } from "react-router-dom"
 
 const ProtectedRouterLayout = () => {
 
